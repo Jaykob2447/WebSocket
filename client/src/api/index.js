ef5f8d0c2d450faef80078e9927d541d0ebebdb8
Viktoriia <freshitpte@gmail.com>
2024-11-03 17:09:18 +0200

    Add socket on client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 const axiosOptions = {
   baseURL: 'http://127.0.0.1:5000/api',
@@ -10,3 +11,21 @@ export const getMessages = limit => apiInstance.get(`/messages?limit=${limit}`);
 
 export const createMessage = newMessage =>
   apiInstance.post('/messages', newMessage);
+
+// -------------------
+const socketClient = io('ws://localhost:5000'); // 'connection'
+// socketClient.emit('connection', socketClient)
+
+socketClient.on('HELLO_TO_ME', () => {
+  console.log('Hello in our chat');
+});
+
+socketClient.on('NEW_PARTICIPANT', () => {
+  console.log('New participant come in');
+});
+
+socketClient.on('CHAT_WILL_CLOSED', () => {
+  console.log('We will closed in a few minutes');
+});
+
+socketClient.emit('SEND_TO_SERVER', 'Message');
