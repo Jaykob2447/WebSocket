d09e5b70f617c0f74d41b771fed3c85b57327c2f
Viktoriia <freshitpte@gmail.com>
2024-11-03 18:45:02 +0200

    Refactor server^ split http&socket server

--- server/index.js
+++ server/index.js
@@ -1,33 +1,12 @@
 const http = require('http');
-const { Server } = require('socket.io');
 const app = require('./app');
-const { Message } = require('./models');
+const initSocket = require('./socket');
 
 const PORT = process.env.PORT ?? 5000;
 
 const httpServer = http.createServer(app);
 
-// ----------------
-const cors = { origin: '*' };
-const wsServer = new Server(httpServer, { cors });
-
-wsServer.on('connection', socket => {
-  console.log('Connection established');
-
-  socket.on('NEW_MESSAGE', async payload => {
-    try {
-      // закинути повідомлення в базу
-      const createdMessage = await Message.create(payload);
-      // успіх - відправити повідомлення всім
-      wsServer.emit('NEW_MESSAGE_SUCCESS', createdMessage);
-    } catch (error) {
-      // помилка - відправити помилку собі
-      socket.emit('NEW_MESSAGE_ERROR', error);
-    }
-  });
-});
-
-// --------------
+initSocket(httpServer);
 
 httpServer.listen(PORT, () => {
   console.log(`Server is running!`);
