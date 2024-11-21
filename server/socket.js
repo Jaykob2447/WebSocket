70b677e978c50c79149d0fa3def179a0262a0c2b
unknown <mrsolomka2447@gmail.com>
2024-11-21 02:02:33 +0200

    create server side to delete message

--- server/socket.js
+++ server/socket.js
@@ -1,23 +1,39 @@
-const { Server } = require('socket.io');
-const { Message } = require('./models');
+const { Server } = require("socket.io");
+const { Message } = require("./models");
 const {
-  SOCKET_SERVER_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_SUCCESS, NEW_MESSAGE_ERROR },
-} = require('./constants');
+  SOCKET_SERVER_EVENTS: {
+    NEW_MESSAGE,
+    NEW_MESSAGE_SUCCESS,
+    NEW_MESSAGE_ERROR,
+    DELETE_MESSAGE,
+    DELETE_MESSAGE_SUCCESS,
+    DELETE_MESSAGE_ERROR,
+  },
+} = require("./constants");
 
-const cors = { origin: '*' };
+const cors = { origin: "*" };
 
-function initSocket (httpServer) {
+function initSocket(httpServer) {
   const wsServer = new Server(httpServer, { cors });
 
-  wsServer.on('connection', socket => {
-    console.log('Connection established');
+  wsServer.on("connection", (socket) => {
+    console.log("Connection established");
 
-    socket.on(NEW_MESSAGE, async payload => {
+    socket.on(NEW_MESSAGE, async (payload) => {
       try {
         const createdMessage = await Message.create(payload);
         wsServer.emit(NEW_MESSAGE_SUCCESS, createdMessage);
       } catch (error) {
         socket.emit(NEW_MESSAGE_ERROR, error);
+      }
+    });
+
+    socket.on(DELETE_MESSAGE, async (payload) => {
+      try {
+        await Message.findByIdAndDelete(payload);
+        wsServer.emit(DELETE_MESSAGE_SUCCESS, payload);
+      } catch (error) {
+        socket.emit(DELETE_MESSAGE_ERROR, error);
       }
     });
   });
